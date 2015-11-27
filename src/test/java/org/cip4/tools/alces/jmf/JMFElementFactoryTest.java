1e17eb6f547a3a947118969f6ecfb9fb24678417
Alex Khilov <khilov@gmail.com>
2015-11-27 10:57:43 +0200

    ALCES-1: Fix unit tests, remove spaces

--- src/test/java/org/cip4/tools/alces/jmf/JMFElementFactoryTest.java
+++ src/test/java/org/cip4/tools/alces/jmf/JMFElementFactoryTest.java
@@ -17,16 +17,16 @@ import org.cip4.tools.alces.test.tests.CheckJDFTest;
 public class JMFElementFactoryTest extends AlcesTestCase {
 
     public void testCreateJMF() throws Exception {
-        JMFMessageFactory factory = JMFMessageFactory.getInstance();        
+        JMFMessageFactory factory = JMFMessageFactory.getInstance();
         JDFJMF jmf = factory.createJMF();
         assertNotNull(jmf);
 
-        String jmfString = jmf.getOwnerDocument_KElement().write2String(2);                
+        String jmfString = jmf.getOwnerDocument_KElement().write2String(2);
         System.out.println(jmfString);
         
         InMessage msg = new InMessageImpl(null, jmfString, true);
         CheckJDFTest test = new CheckJDFTest();
-        TestResult result = test.runTest(msg);        
+        TestResult result = test.runTest(msg);
         assertTrue("Message was not valid: " + jmfString, result.isPassed());
                     
         System.out.println(result.getResultString());
@@ -35,7 +35,7 @@ public class JMFElementFactoryTest extends AlcesTestCase {
     public void testCreateQueryKnownMessages() throws Exception {
         JDFJMF jmf = JMFMessageFactory.getInstance().createJMF("QueryKnownMessages");
 
-        String jmfString = jmf.getOwnerDocument_KElement().write2String(2);                
+        String jmfString = jmf.getOwnerDocument_KElement().write2String(2);
         System.out.println(jmfString);
         
         InMessage msg = new InMessageImpl(null, jmfString, true);
@@ -56,7 +56,7 @@ public class JMFElementFactoryTest extends AlcesTestCase {
     public void testCreateIncorrectXsiType() throws Exception {
         boolean failed = false;
         try {
-            JMFMessageFactory.getInstance().createJMF("asdf");    
+            JMFMessageFactory.getInstance().createJMF("asdf");
         } catch (Exception e) {
             failed = true;
         }
